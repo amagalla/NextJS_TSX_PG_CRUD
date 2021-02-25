09ac2453cd224609cbedd971648eda00c300b966
amagalla <anthonymagallanes24@gmail.com>
2021-02-24 20:26:04 -0800

    removed .next and added compnentmponent and styles folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React, { FunctionComponent } from "react";
+import GetLists from "../src/components/GetLists";
 
 const App: FunctionComponent = () => {
-  return <div>This is App</div>;
+  return (
+    <>
+      <div>This is App</div>
+      <GetLists />
+    </>
+  );
 };
 
 export default App;
